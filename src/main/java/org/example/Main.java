e1dff73365cb96325fd1d99e0d88fe80b1a56a40
isaacquinapallo <isaac.quinapallo@epn.edu.ec>
2024-05-30 09:07:21 -0500

    Instrucciones

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,13 +1,21 @@
 package org.example;
 
+import Procesadores.Procesadores_AMD;
+import Procesadores.Procesadores_moviles;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //dennis diaz
-        //hola a todos
-        //HOLA//
-        //HOLAA///
+        //Integrante1
+        Procesadores_AMD proc_1 = new Procesadores_AMD();
+        Procesadores_AMD proc_2 = new Procesadores_AMD();
+        //Integrante2
+        Procesadores_moviles proc_3 = new Procesadores_moviles();
+        Procesadores_moviles proc_4 = new Procesadores_moviles();
+
+        //Dennis por favor crear 2 instancias de intel con valores
+
         System.out.println("Hello World");
         System.out.println("ellloo");
     }
